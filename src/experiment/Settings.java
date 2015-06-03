77ff76cfd3b47cea9be2e51d4975473a85a901d9
jacksonpradolima <pradolima@live.com>
2015-06-03 15:30:04 -0300

    Nova configuração do problema

--- src/experiment/Settings.java
+++ src/experiment/Settings.java
@@ -13,47 +13,46 @@ package experiment;
 public class Settings {
 
     public static final String[] INSTANCES = {
-        //"instances/cas",
-        //"instances/eshop",
-        //"instances/james",
+        "instances/cas",
+        "instances/eshop",
+        "instances/james",
         "instances/weatherstation"
     };
 
     public static final MetaheuristicType[] ALGORITHMS = {
-        //MetaheuristicType.NSGAII,
+        MetaheuristicType.NSGAII,
         MetaheuristicType.NSGAIII
     };
 
     public static final int[] POPULATION_SIZE = {
-        50//,
-        //100,
-        //200
+        50,
+        100,
+        200
     };
 
     public static final int[] GENERATIONS = {
-        100//,
-        //300,
-        //600
+        100,
+        300,
+        600
     };
 
     public static final double[] CROSSOVER_PROBABILITY = {
-        0.8//,
-        //0.9
+        0.8,
+        0.9
     };
 
     public static final double[] MUTATION_PROBABILITY = {
-        0.005
-        //0.01
-        //0.1
+        0.005,
+        0.01
     };
 
     public static final String[] CROSSOVER_OPERATORS = {
-        //"SinglePointCrossover",
+        "SinglePointCrossover",
         "UniformCrossoverBinary"
     };
 
     public static final String[] MUTATION_OPERATORS = {
-        //"BitFlipMutation",
+        "BitFlipMutation",
         "SwapMutationBinary"
     };
 
@@ -61,5 +60,5 @@ public class Settings {
         "BinaryTournament2"
     };
 
-    public static final int EXECUTIONS = 1;
+    public static final int EXECUTIONS = 5;
 }
